de1fb3a1ae118fa6db8e7050745226c9bb7a6fed
Duc Nguyen Viet <59314388+tomgermanyyyy@users.noreply.github.com>
2023-06-20 15:46:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,4 +29,12 @@ app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/users', async (req, res) => {
+  // Use basic logger without HTTP request info
+  logger.info({logField: 'custom-entry', arbitraryField: 'custom-entry'}); // Example of structured logging
+  // Use request-based logger with log correlation
+  req.log.info('Child logger with trace Id.'); // https://cloud.google.com/run/docs/logging#correlate-logs
+  res.send('Users');
+});
+
 module.exports = app;
